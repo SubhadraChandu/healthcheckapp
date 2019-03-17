7ee560112784dde4b62fefd47fc95b4f81847ae7
Subhadra <subhadra.chandu@gmail.com>
2019-03-17 16:45:26 +1100

    health check app

--- src/test/java/com/healthcheck/healthcheckapplication/HealthcheckapplicationApplicationTests.java
+++ src/test/java/com/healthcheck/healthcheckapplication/HealthcheckapplicationApplicationTests.java
@@ -2,6 +2,7 @@ package com.healthcheck.healthcheckapplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,12 @@ public class HealthcheckapplicationApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	@Test
+	public void up() {
+		Health health = new Health.Builder().up().build();
+		//assertThat(health.getStatus()).isEqualTo(Status.UP);
+		//assertThat(health.getDetails()).isEmpty();
 	}
 
 }
